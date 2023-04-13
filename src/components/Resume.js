b43c8cf8d67ab01e48b67d6c7e576f4c6a61b7c0
Elvis Diaz <elvisdiaz020@gmail.com>
2023-04-13 02:15:07 -0400

    adds resume styling, mobile and tablet media queries, and adds downloadable pdf link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,24 +1,33 @@
 import React from "react";
+import resume from "../images/resume.pdf";
 
 const Resume = () => {
   return (
-    <div>
-      <span>Front End Specialities</span>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>JQuery</li>
-        <li>React</li>
-      </ul>
-      <span>Back End Specialities</span>
-      <ul>
-        <li>APIs</li>
-        <li>Node.js</li>
-        <li>MySQL & Sequelize</li>
-        <li>MongoDB & Mongoose</li>
-        <li>Express</li>
-      </ul>
+    <div id="resume-container">
+      <div id="resume">
+        <span>Front End Specialities</span>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>JQuery</li>
+          <li>React</li>
+        </ul>
+        <span>Back End Specialities</span>
+        <ul>
+          <li>APIs</li>
+          <li>Node.js</li>
+          <li>MySQL & Sequelize</li>
+          <li>MongoDB & Mongoose</li>
+          <li>Express</li>
+        </ul>
+        <p id="resume-download">
+          Check out my resume!{" "}
+          <a id="resume-download-link" href={resume} download>
+            Download Resume
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
